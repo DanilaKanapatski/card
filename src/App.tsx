4819ad9d989ce474c2ab9fca7e021dd73a7886b0
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-24 00:34:16 +0300

    done card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import styled from "styled-components";
+import {GlobalStyles} from "./styles/GlobalStyles";
+import {Button} from "./components/Button.styled";
+import {myTheme} from "./styles/Theme.styled";
+import logo from "./cardImage.png"
+import {Link} from "./components/Link.styled";
+import {CardTitle} from "./components/CardTitle.styled";
+import {CardText} from "./components/CardText.styled";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <>
+            <GlobalStyles></GlobalStyles>
+            <Box>
+                <img src={logo} alt='Card'/>
+                <ContentWrapper>
+                    <CardTitle>Headline</CardTitle>
+                    <CardText>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in
+                        venen.</CardText>
+                    <Button as={Link} href={'#'}>See more</Button>
+                    <Button>Save</Button>
+                </ContentWrapper>
+            </Box>
+        </>
+    );
 }
 
 export default App;
+
+const Box = styled.div`
+    width: 300px;
+    height: 350px;
+    margin: 176px auto;
+    padding: 10px 10px 22px;
+    border-radius: 15px;
+    background-color: ${myTheme.colors.card};
+    box-shadow: 0 4px 20px 5px ${myTheme.colors.shadow};
+    
+    &:hover {
+        zoom: calc(1.1);
+    }
+`
+
+const ContentWrapper = styled.div`
+    margin-top: 20px;
+    padding: 0 0 0 10px;
+`
+
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider} from "styled-components";
+import {GlobalStyles} from "./styles/GlobalStyles";
+import {myTheme} from "./styles/Theme.styled";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={myTheme}>
+      <App />
+      <GlobalStyles />
+  </ThemeProvider>
 );
 
 reportWebVitals();
