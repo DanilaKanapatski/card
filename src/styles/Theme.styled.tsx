c128c8a56be87d644e0bd74a112b3d6ffc11f33e
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-24 00:33:01 +0300

    added theme styles

---
+++ src/styles/Theme.styled.tsx
@@ -0,0 +1,10 @@
+export  const myTheme = {
+    colors: {
+        text: "#ABB3BA",
+        card: "#FFFFFF",
+        title: "#000000",
+        button: "#4E71FE",
+        body: "#F5F5F5",
+        shadow: 'rgba(000,000,000,0.1)',
+    }
+}
